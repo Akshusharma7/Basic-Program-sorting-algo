3a38665dc8fe1eec0484a1afd7a3959bbb103ab6
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-11-19 07:25:04 +0530

    Generator
    A Python generator is a function which returns a generator iterator (just an object we can iterate over) by calling yield . yield may be called with a value, in which case that value is treated as the "generated" value.

---
+++ Generator.py
@@ -0,0 +1,41 @@
+import mem_profile
+import random
+import time
+
+name = ['John', 'Corey', 'Adam','Steve','Rick', 'Thomas']
+majors = ['Math','Engineering','CompSci','Arts','Business']
+
+print("Memory (Before): {}Mb".format(mem_profile.memory_usage_resource()))
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {'id' : i,
+                  'name' : random.choice(name),
+                  'major' : random.choice(major)
+                  }
+        result.append(person)
+        return result
+
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {'id' : i,
+                  'name' : random.choice(name),
+                  'major' : random.choice(major)
+                  }
+        
+        yield person
+#Using List 
+t1 = time.clock()
+people = people_list(1000000)
+t2 = time.clock()
+
+#Using Generator
+t1 = time.clock()
+people = people_generator(1000000)
+t2 = time.clock()
+
+
+print("Memory (After): {}Mb".format(mem_profile.memory_usage_resource()))
+print('Total {} seconds'.format(t2-t1))
