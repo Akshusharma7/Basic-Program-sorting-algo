607448c3d93cf19c707f9afab5821ddc43a56e95
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-02 16:43:18 +0530

    Update coin_row_count.py

--- coin_row_count.py
+++ coin_row_count.py
@@ -1,4 +1,6 @@
-
+#HackerRank problem
+#Count the number of row possibe after aranging the coins in shape of stairs.
+#suppose there are 5 coins then possible stairs will be 2 and if there are 15 coins the stairs will be 5.
 
 
 
