328f6bf907d2ee4950227e91a0aff05119a3f538
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-08 23:29:47 +0530

    Create kth_elment_from_end.py

---
+++ kth_elment_from_end.py
@@ -0,0 +1,15 @@
+arr = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19]
+k = 5
+def last_elem(arr):
+    p = 0
+    q = k-1
+    
+    for i in range(100):
+        p +=1
+        q +=1
+        if q == len(arr):
+            break
+    #print("element is : ", arr[p-1])
+    return arr[p-1]
+
+last_elem(arr)
