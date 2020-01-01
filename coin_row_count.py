42267c91632e5f8db1ed8078075e9ee3fe02ebc7
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-01 21:54:49 +0530

    Update coin_row_count.py

--- coin_row_count.py
+++ coin_row_count.py
@@ -3,7 +3,6 @@
 #suppose there are 5 coins then possible stairs will be 2 and if there are 15 coins the stairs will be 5.
 
 
-
 coins = int(input("Enter the coins : "))#5 --> 2
 temp = coins
 for row in range(1,coins):
