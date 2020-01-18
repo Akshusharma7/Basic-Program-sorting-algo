5787087cbc054809fc9f6dcbf2220e9104cc6ef3
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-18 08:08:41 +0530

    Update sh_modulw.py

--- sh_modulw.py
+++ sh_modulw.py
@@ -2,8 +2,12 @@ import sh
 
 #call system commands
 sh.pwd() #Get current Directory
+
 sh.mkdir("files") #Make a new Directory
+
 sh.touch("password.txt") #Make a new File
+
 sh.whoami() #Get current user
+
 sh.echo("Hello we are using 'sh' module")
 
