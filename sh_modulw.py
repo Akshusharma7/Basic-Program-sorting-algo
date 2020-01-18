cb1f944527f26651dd14950c4fba6d50347caa64
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-18 08:09:00 +0530

    Update sh_modulw.py

--- sh_modulw.py
+++ sh_modulw.py
@@ -2,9 +2,7 @@ import sh
 
 #call system commands
 sh.pwd() #Get current Directory
-
 sh.mkdir("files") #Make a new Directory
-
 sh.touch("password.txt") #Make a new File
 
 sh.whoami() #Get current user
