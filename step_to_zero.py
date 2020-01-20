7ef28950030e1d9d72ef281c0fd6adbf7ba5698d
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-20 08:59:48 +0530

    Update step_to_zero.py

--- step_to_zero.py
+++ step_to_zero.py
@@ -4,3 +4,16 @@ def binary_to_decimal(s):
         if int(x) == 1:
             dec = dec+2**i
     return dec
+
+def step_to_zero(s):
+    count = 0
+    n = binary_to_decimal(s)
+    while n>0:
+        if n%2==0:
+            n = n//2
+        else:
+            n = n-1
+        count +=1
+    return count
+
+step_required = step_to_zero("011100") #28
