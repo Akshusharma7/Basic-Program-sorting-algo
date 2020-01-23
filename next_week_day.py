5fdb86e08c72efc68eab434052ba0f6b17a8f6cb
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-23 09:08:59 +0530

    Create next_week_day.py

---
+++ next_week_day.py
@@ -0,0 +1,9 @@
+def week_num(day,n):
+    week = ["mon","tue","wed","thu","fri","sat","sun"]
+    ind = week.index(day)
+
+    next_day = (n+ind)%7
+    days = week[next_day]
+    return days
+
+print(week_num("tue",2))
