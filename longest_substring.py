bc94eabcb391edf6c851895a7a1ff0dbc677dd88
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2022-01-12 10:16:53 +0530

    Create longest_substring.py

---
+++ longest_substring.py
@@ -0,0 +1,25 @@
+"""
+This problem was asked by Amazon.
+
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+"""
+
+def longest_substring_with_k_distinct_characters(character,k):
+
+    current_length = ''
+    for i in range(len(character)):
+        for j in range(i+1,len(character)+1):
+            substring = character[i:j]
+            if len(set(substring)) <= k and len(substring) > len(current_length):
+                current_length = substring
+    return current_length
+    
+
+if __name__ == '__main__':
+    s = "abcba"
+    k = 2
+    out = longest_substring_with_k_distinct_characters(s,k)
+    print(out)
